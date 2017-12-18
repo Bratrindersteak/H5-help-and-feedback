666633290b133dcee29d94fd910f4c26a6c37e24
MaxWu <2386140382@qq.com>
2017-12-18 16:04:48 +0800

    upload

--- src/reducers/im/description/count.js
+++ src/reducers/im/description/count.js
@@ -1,9 +1,9 @@
-import { WRITE_DESCRIPTION } from '../../../actions/send/description';
+import { WRITING_DESCRIPTION } from '../../../actions/send/description';
 import SEND from '../../../jsons/send.json';
 
 const count = (state = SEND.description.count, action) => {
     switch (action.type) {
-        case WRITE_DESCRIPTION:
+        case WRITING_DESCRIPTION:
             return Object.assign({}, state, {
                 current: action.payload.count,
             });
